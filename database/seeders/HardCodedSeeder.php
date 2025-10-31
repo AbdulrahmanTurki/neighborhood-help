88f20965eff9249a4fbe4f2a758c6d46cea8c62f
AbdulrahmanTurki <azt.2004@hotmail.com>
2025-10-31 04:23:13 +0000

    First commit : Migrations

---
+++ database/seeders/HardCodedSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class HardCodedSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
